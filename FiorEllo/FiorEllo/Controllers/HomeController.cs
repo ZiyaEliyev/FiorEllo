7ac90d3b7234c0a50b4576599c3fde053ce9cbc6
ZiyaEliyev <ziyaeliyev522@gmail.com>
2023-05-16 20:57:00 +0400

    Init

---
+++ FiorEllo/FiorEllo/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using FiorEllo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FiorEllo.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+      
+
+       
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ FiorEllo/FiorEllo/DAL/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace FiorEllo.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            
+        }
+    }
+}
