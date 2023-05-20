45534bc56de4c13dfe75b6bf019f99aa06c8e309
ZiyaEliyev <ziyaeliyev522@gmail.com>
2023-05-20 23:53:01 +0400

    I created Update for Products in Admin Panel

--- FiorEllo/FiorEllo/Areas/Admin/Controllers/ProductsController.cs
+++ FiorEllo/FiorEllo/Areas/Admin/Controllers/ProductsController.cs
@@ -22,25 +22,25 @@ namespace FiorEllo.Areas.Admin.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            List<Product> products = await _db.Products.Include(x=>x.Category).ToListAsync();
+            List<Product> products = await _db.Products.Include(x => x.Category).ToListAsync();
             return View(products);
         }
 
         public async Task<IActionResult> Create()
         {
-           ViewBag.Categories = await _db.Categories.ToListAsync();
+            ViewBag.Categories = await _db.Categories.ToListAsync();
             return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
 
-        public async Task<IActionResult> Create(Product product,int catId)
+        public async Task<IActionResult> Create(Product product, int catId)
         {
-           ViewBag.Categories = await _db.Categories.ToListAsync();
+            ViewBag.Categories = await _db.Categories.ToListAsync();
 
             #region isExist
-            bool isExist =await _db.Products.AnyAsync(x=>x.Name == product.Name);
+            bool isExist = await _db.Products.AnyAsync(x => x.Name == product.Name);
             if (isExist)
             {
                 ModelState.AddModelError("Name", "This product is already exist !");
@@ -71,6 +71,73 @@ namespace FiorEllo.Areas.Admin.Controllers
             await _db.Products.AddAsync(product);
             await _db.SaveChangesAsync();
             return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Update(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Product dbProduct = await _db.Products.FirstOrDefaultAsync(x => x.Id == id);
+            if (dbProduct == null)
+            {
+                return BadRequest();
+            }
+            ViewBag.Categories = await _db.Categories.ToListAsync();
+            return View(dbProduct);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(int? id, Product product, int catId)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Product dbProduct = await _db.Products.FirstOrDefaultAsync(x => x.Id == id);
+            if (dbProduct == null)
+            {
+                return BadRequest();
+            }
+            ViewBag.Categories = await _db.Categories.ToListAsync();
+
+            #region isExist
+            bool isExist = await _db.Products.AnyAsync(x => x.Name == product.Name && x.Id!=id);
+            if (isExist)
+            {
+                ModelState.AddModelError("Name", "This product is already exist !");
+                return View();
+            }
+            #endregion
+
+            #region Save Image
+            if (product.Photo != null)
+            {
+                if (!product.Photo.IsImage())
+                {
+                    ModelState.AddModelError("Photo", "Please select image type file !");
+                    return View();
+                }
+                if (product.Photo.IsOlder1Mb())
+                {
+                    ModelState.AddModelError("Photo", "File size can be max 1Mb !");
+                    return View();
+                }
+                string folder = Path.Combine(_env.WebRootPath, "img");
+                dbProduct.Image = await product.Photo.SaveFileAsync(folder);
+            }
+            #endregion
+
+            dbProduct.Name= product.Name;
+            dbProduct.Price= product.Price;
+            dbProduct.CategoryId= catId;
+
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+           
         }
     }
 }
